c7159b2a23f4b7c1c631e856f263b666a55867d1
BostonWafro <wafrogaming@gmail.com>
2021-01-15 12:28:28 +0000

    nearly done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let floatArr = [];
+console.log(floatArr)
+
+class App extends React.Component {
+  state = {
+    currentName: "",
+    currentNum: "",
+    inputName: "",
+    inputNum: "",
+    addArr: [],
+  };
+
+  nameUpdate = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({ [nam]: val })
+  }
+  
+  buttonClick = (event) => {
+    event.preventDefault()
+    this.setState({ inputName: this.state.currentName })
+    this.setState({ inputNum: this.state.currentNum })
+    let person = {"name": `${this.state.inputName}`, "number": `${this.state.inputNum}`}
+    if (this.state.currentName !== "" && this.state.currentNum !== "") {
+      floatArr.push(person);
+    }
+    this.setState({ addArr: floatArr })
+    console.log(this.state.addArr)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="inputs">
+          <form>
+            <input className="inputBox" type="text" name="currentName" onChange={this.nameUpdate}/>
+            <input className="inputBox" type="text" name="currentNum" onChange={this.nameUpdate}/>
+            <button className="submit" onClick={this.buttonClick}></button>
+          </form>
+          {this.state.addArr.map((item, index) => {
+            return <p key={index}>{item.name}</p>
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
